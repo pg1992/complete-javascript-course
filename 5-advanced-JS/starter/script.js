4e82a8856f3effbff0b36b9cceeb649675cbe150
Pedro Guilherme Siqueira Moreira <pedro.guilherme@espectro.eng.br>
2020-03-15 15:10:48 -0300

    Play with constructors, protorypes, and prototypes chains

---
+++ 5-advanced-JS/starter/script.js
@@ -0,0 +1,30 @@
+var john = {
+  name: "John",
+  yearOfBirth: 1990,
+  job: "teacher"
+};
+
+var Person = function(name, yearOfBirth, job) {
+  this.name = name;
+  this.yearOfBirth = yearOfBirth;
+  this.job = job;
+};
+
+Person.prototype.calculateAge = function() {
+  console.log(2020 - this.yearOfBirth);
+};
+
+Person.prototype.lastName = 'Smith';
+
+var john = new Person("John", 1990, "teacher");
+john.calculateAge();
+
+var jane = new Person("Jane", 1969, "designer");
+var mark = new Person("Mark", 1948, "retired");
+
+jane.calculateAge();
+mark.calculateAge();
+
+console.log(john.lastName);
+console.log(jane.lastName);
+console.log(mark.lastName);
