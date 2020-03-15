3fc18b88443bf45d4541988eddeb26788b6dece4
Pedro Guilherme Siqueira Moreira <pedro.guilherme@espectro.eng.br>
2020-03-15 15:30:40 -0300

    Use Object.create to create prototypes

--- 5-advanced-JS/starter/script.js
+++ 5-advanced-JS/starter/script.js
@@ -1,3 +1,5 @@
+// Function.prototype
+/*
 var Person = function(name, yearOfBirth, job) {
   this.name = name;
   this.yearOfBirth = yearOfBirth;
@@ -21,3 +23,22 @@ mark.calculateAge();
 console.log(john.lastName);
 console.log(jane.lastName);
 console.log(mark.lastName);
+*/
+
+// Object.create
+var personProto = {
+  calculateAge: function() {
+    console.log(2020 - this.yearOfBirth);
+  }
+};
+
+var john = Object.create(personProto);
+john.name = "John";
+john.yearOfBirth = 1990;
+john.job = "teacher";
+
+var jane = Object.create(personProto, {
+  name: { value: "Jane" },
+  yearOfBirth: { value: 1969 },
+  job: { value: "designer" }
+});
