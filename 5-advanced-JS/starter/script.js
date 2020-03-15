dbe43571881f82867bf4cdd222083cd34190d399
Pedro Guilherme Siqueira Moreira <pedro.guilherme@espectro.eng.br>
2020-03-15 15:20:07 -0300

    Minor refactor

--- 5-advanced-JS/starter/script.js
+++ 5-advanced-JS/starter/script.js
@@ -1,9 +1,3 @@
-var john = {
-  name: "John",
-  yearOfBirth: 1990,
-  job: "teacher"
-};
-
 var Person = function(name, yearOfBirth, job) {
   this.name = name;
   this.yearOfBirth = yearOfBirth;
@@ -14,14 +8,13 @@ Person.prototype.calculateAge = function() {
   console.log(2020 - this.yearOfBirth);
 };
 
-Person.prototype.lastName = 'Smith';
+Person.prototype.lastName = "Smith";
 
 var john = new Person("John", 1990, "teacher");
-john.calculateAge();
-
 var jane = new Person("Jane", 1969, "designer");
 var mark = new Person("Mark", 1948, "retired");
 
+john.calculateAge();
 jane.calculateAge();
 mark.calculateAge();
 
