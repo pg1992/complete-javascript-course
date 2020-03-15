1793b588a6b92f1453373e0fe99017ced1b9db39
Pedro Guilherme Siqueira Moreira <pedro.guilherme@espectro.eng.br>
2020-03-15 15:37:51 -0300

    Changes between primitives and objects
    Primitives are passed to functions by values and
    objects are passed to functions by reference.

--- 5-advanced-JS/starter/script.js
+++ 5-advanced-JS/starter/script.js
@@ -26,6 +26,7 @@ console.log(mark.lastName);
 */
 
 // Object.create
+/*
 var personProto = {
   calculateAge: function() {
     console.log(2020 - this.yearOfBirth);
@@ -42,3 +43,44 @@ var jane = Object.create(personProto, {
   yearOfBirth: { value: 1969 },
   job: { value: "designer" }
 });
+*/
+
+// Primitives vs Objects
+
+// Primitives
+var a = 23;
+var b = a;
+
+a = 46;
+
+console.log(a);
+console.log(b);
+
+// Objects
+var obj1 = {
+  name: "John",
+  age: 26
+};
+
+var obj2 = obj1;
+obj1.age = 30;
+
+console.log(obj1.age);
+console.log(obj2.age);
+
+// Functions
+var age = 27;
+var obj = {
+  name: "Jonas",
+  city: "Lison"
+};
+
+function change(a, b) {
+  a = 30; // pass by value
+  b.city = "San Francisco"; // pass by reference
+}
+
+change(age, obj);
+
+console.log(age);
+console.log(obj.city);
