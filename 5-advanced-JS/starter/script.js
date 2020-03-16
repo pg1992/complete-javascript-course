dc2504d4171f55679e8e9ea03fa4cf104fa29cc2
Pedro Guilherme Siqueira Moreira <pedro.guilherme@espectro.eng.br>
2020-03-15 23:51:07 -0300

    Returning functions
    Decorators prequel :)

--- 5-advanced-JS/starter/script.js
+++ 5-advanced-JS/starter/script.js
@@ -89,7 +89,7 @@ console.log(obj.city);
 
 //////////////////////////////////////////
 // Lecture: Passing functions as arguments
-
+/*
 var years = [1990, 1965, 1937, 2005, 1998, 1992];
 
 function arrayCalc(arr, fn) {
@@ -123,3 +123,34 @@ var rates = arrayCalc(ages, maxHeartRate);
 console.log(ages);
 console.log(fullAges);
 console.log(rates);
+*/
+
+//////////////////////////////
+// Lecture: Functions returning functions
+
+function interviewQuestion(job) {
+  if (job === "designer") {
+    return function(name) {
+      console.log(name + ", can you please what UX design is?");
+    };
+  } else if (job === "teacher") {
+    return function(name) {
+      console.log("What subject do you teach, " + name + "?");
+    };
+  } else {
+    return function(name) {
+      console.log("Hello " + name + ", what do you do?");
+    };
+  }
+}
+
+var teacherQuestion = interviewQuestion("teacher");
+var designerQuestion = interviewQuestion("designer");
+
+teacherQuestion("John");
+designerQuestion("John");
+designerQuestion("Jane");
+designerQuestion("Mark");
+designerQuestion("Mike");
+
+interviewQuestion("teacher")("Mark");
